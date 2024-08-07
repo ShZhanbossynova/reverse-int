bdc465deb53d184cd1cdfe9cd291591421a4d0b1
Shinar Zhanbosynova <sk.zhanbossynova@gmail.com>
2024-08-08 00:56:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+function reverse (n) {
+    let result = ''
+    let absolute = Math.abs(n)
+    let absstring = String(absolute)
+
+    for (let i = 0; i < absstring.length; i ++) {
+        result = `${absstring[i]}${result}`
+    }
+    console.log(+result)
 }
